a0a0b5714acecf9e8d9e112fd77a4ae0c4f87c00
deibson.januario <deibson.januario@gmail.com>
2025-05-12 22:18:20 -0300

    ajuste das imagens

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION["etapa"]) || $_SESSION["etapa"] == "0"){ //Selecionar a quan
     	</br></br>
     	<div class="row">
         <?php
-        for ($i = 1; $i < 7; $i++) {
+        for ($i = 3; $i < 7; $i++) {
             $html  = '<div class="col-md-6">';
             $html .=      '<a href="./index.php?etapa=1&numero='.$i.'"><img width="400" src="./assets/imagens/numero-jogadores/' . $i . '.png" class="img-fluid botao-total-jogadores" onclick="btnVibrate();"></a>';
             $html .= '</div>';
